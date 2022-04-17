22c01ca3ef88608bb26262b9db78b3f10c5b5093
ggggght <wz9712203617@gmail.com>
2022-04-17 16:32:30 +0800

    feat: spring native

---
+++ src/main/java/com/ggggght/extensibility/ExtensibilityApplication.java
@@ -0,0 +1,12 @@
+package com.ggggght.extensibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensibilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtensibilityApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ggggght/extensibility/ExtensibilityApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ggggght.extensibility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtensibilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
